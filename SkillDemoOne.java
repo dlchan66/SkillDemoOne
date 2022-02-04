be36eaf413d8cfd09a48e62d8172ff5e59cac4f0
dlchan66 <dlchan@ucsd.edu>
2022-02-04 15:05:23 -0800

    create lib and failing test

--- SkillDemoOne.java
+++ SkillDemoOne.java
@@ -1,3 +1,5 @@
 public class SkillDemoOne {
-
+    public static int squared(int n) {
+        return n * n;
+    }
 }
---
+++ SkillDemoOneTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoOneTest {
+    @Test
+    public void testSquared() {
+        assertEquals(25, SkillDemo1.squared(4));
+    }
+}
